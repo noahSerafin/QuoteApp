281cd7df1bf8bdfa445d3c90457bd6af51a256ca
noah.serafin <noah.serafin@DESKTOP-D0OO5CA>
2024-05-15 12:30:39 +0100

    Add project files.

---
+++ WebApplication1/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿namespace QuoteWebApp.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string QuoteText { get; set; }
+        public string QuoteAnswer { get; set; }
+
+        public Quote()
+        {
+            
+        }
+    }
+}
